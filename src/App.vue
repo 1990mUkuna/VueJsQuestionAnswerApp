e75660bdd434cadde626682c86f2e4605acebbad
MukunaFullstack <mukuna.freeman@gmail.com>
2020-07-20 09:43:08 +0200

    Initial commit, display questions

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,61 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app"> 
+     <Header/> 
+     <b-container class="bv-example-row">
+      <b-row>
+        <b-col sm="6" offset="3">
+             <QuestionBox 
+              :currentQuestion="questions[index]"
+              /> 
+        </b-col> 
+      </b-row>
+  </b-container> 
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+ import Header from './components/Header.vue'
+ import QuestionBox from './components/QuestionBox.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+  Header,
+  QuestionBox
+  },
+  data() {
+    return {
+       questions: [],
+       index: 0
+    }
+  },
+  mounted: function(){
+    // mounted hook used lifecycle hook in vue, Vue calls the mounted() hook when component is added to the DOM
+    // Often used to send an Http request to fetch data tha the component will render
+    fetch('https://opentdb.com/api.php?amount=10&category=19&type=multiple', {
+      method: 'get'
+    })
+    //Taking the response
+    .then ((response) => {
+    return response.json()
+    })
+    .then((jsonData) => {
+   // Accessing data for the application
+    this.questions = jsonData.results
+    })
   }
 }
 </script>
 
 <style>
+ 
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  -moz-osx-font-smoothing:grayscale;
+ text-align: center;
+  color: brown;
+  margin-top:60px;
 }
 </style>
---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="question-box-container">
+           <b-jumbotron> 
+               <template v-slot:lead>
+               {{currentQuestion.question}}
+               </template> 
+               <hr class="my-4"> 
+               <p>
+                  Answers here:
+               </p> 
+               <b-button variant="primary" href="#">Submit</b-button>
+               <b-button variant="success" href="#">Next</b-button> 
+          </b-jumbotron>
+   </div>
+</template>
+
+<script>
+//object
+export default{
+     // In order to display current Question you have to refference it in Pops Object
+    props: {
+         currentQuestion: Object
+    } 
+}
+</script>
